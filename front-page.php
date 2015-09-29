1be454ec8bf2aa69df7a2cadc4b2a31c37ccc70b
Isaac Rdz <zack_cpp@hotmail.com>
2015-09-29 17:37:23 -0500

    nav

--- front-page.php
+++ front-page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<?php include( TEMPLATEPATH . "/partials/main-slider.php") ?>
 <?php include( TEMPLATEPATH . "/partials/ads-1.php") ?>
 <div class="mb10"></div><!-- space -->
 <hr class="mt20 mb70 gray">
